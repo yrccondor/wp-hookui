e1011c8eff5926c3c95167c332ecb6ff07509639
unknown <yrc371106@hotmail.com>
2018-05-04 14:09:58 +0800

    Improve backup

--- class-itemoperator.php
+++ class-itemoperator.php
@@ -50,21 +50,79 @@ class hui_item_operator{
      * Will be deleted when the plugin is activated or in the process of uninstallation
      */
     public function create_backup(){
-        //...
+        $src = 'custom/';
+        $dst = '../../../hui-backup/custom/'
+
+        $dir = opendir($src); 
+        @mkdir($dst); 
+        while(false !== ($file = readdir($dir))){ 
+            if(($file != '.') && $file != '..')){ 
+                if(is_dir($src.'/'.$file)){ 
+                    recurse_copy($src.'/'.$file,$dst. '/'.$file); 
+                } 
+                else { 
+                    copy($src.'/'.$file,$dst.'/'.$file); 
+                } 
+            } 
+        } 
+        closedir($dir);
+        return true;
     }
 
     /**
      * Remove the buckup of functions-custom-0.php and functions-custom-1.php in ../../../hui-backup/custom/ when the plugin is in the process of uninstallation
      */
     public function remove_backup(){
-        //...
+        $dir = '../../../hui-backup/custom/';
+
+        $handle = @opendir($dir);
+        while(($file = readdir($handle)) !== false){   
+            if($file != '.' && $file != '..'){   
+                $dir = $dir.'/'.$file;
+                unlink($dir);
+            }
+        }
+        closedir($handle);
+        return rmdir($dir);
     }
 
      /**
      * Move the buckup of functions-custom-0.php and functions-custom-1.php from ../../../hui-backup/custom/ to ./custom/ when the plugin is activated
      */
     public function cover_backup(){
-        //...
+        //Remove ./custom/
+        $dir = 'custom/';
+
+        $handle = @opendir($dir);
+        while(($file = readdir($handle)) !== false){   
+            if($file != '.' && $file != '..'){   
+                $dir = $dir.'/'.$file;
+                unlink($dir);
+            }
+        }
+        closedir($handle);
+        rmdir($dir);
+
+        //Copy ../../../hui-backup/custom/ to ./custom/
+        $dst = 'custom/';
+        $src = '../../../hui-backup/custom/'
+
+        $dirc = opendir($src); 
+        @mkdir($dst); 
+        while(false !== ($file = readdir($dirc))){ 
+            if(($file != '.') && $file != '..')){ 
+                if(is_dir($src.'/'.$file)){ 
+                    recurse_copy($src.'/'.$file,$dst. '/'.$file); 
+                } 
+                else { 
+                    copy($src.'/'.$file,$dst.'/'.$file); 
+                } 
+            } 
+        } 
+        closedir($dirc);
+
+        //Delete ../../../hui-backup/custom/
+        return $this -> remove_backup();
     }
 }
 ?>
--- uninstall.php
+++ uninstall.php
@@ -11,6 +11,8 @@ delete_option('hui_hash');
 delete_option('hui_options');
 delete_option('hui_init');
 delete_option('hui_version');
+
+//Remove backup
 $hui_item_operator -> set_target('functions-custom-0.php');
 $hui_item_operator -> remove_backup();
 $hui_item_operator -> set_target('functions-custom-1.php');
