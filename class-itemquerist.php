ef879d6ed5b928032b5cd64e069cda95bdbdc572
unknown <yrc371106@hotmail.com>
2018-05-17 12:34:53 +0800

    Improve class

--- class-itemquerist.php
+++ class-itemquerist.php
@@ -24,7 +24,13 @@ class hui_item_querist{
      * return an array of the item's info or FALSE
      */
     public function get_item_info($hui_item_id){
-        //...
+        $item_list = data_to_array();
+        for ($i = 0; $i < sizeof($item_list); $i++){
+            if($item_list[$i]["item_id"] === $hui_item_id){
+                break;
+            }
+        }
+        return $this -> $item_list[$i];
     }
 
     /**
@@ -37,20 +43,24 @@ class hui_item_querist{
 
     /**
      * Read data from target file, convert it to array
-     * return an array or FALSE
+     * return an array of all items' important info or FALSE
      */
     private function data_to_array(){
         $file = fopen($this -> hui_target_file_name, "r");
         $data = array();
+        $item_id = array();
         $item_num = 0;
         $match_sta = 0;
         while(!feof($file)){
             $content = fgets($file)
-            if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-Start-------\d*/is", $content)) && $match_sta === 0){
+            if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-Start-------\d*/is", $content, $item_id)) && $match_sta === 0){
                 $data[$item_num] = array();
+                $data[$item_num]["item_id"] = substr($item_id[0], 7, 40);
                 $match_sta = 1;
             }else if($match_sta === 1){
-                $data[$item_num]["item_id"] = substr($content, 9);
+                if($data[$item_num]["item_id"] !==  substr($content, 9)){
+                    return false;
+                }
                 $match_sta = 2;
             }else if($match_sta === 2){
                 $data[$item_num]["hook_id"] = substr($content, 9);
@@ -80,9 +90,13 @@ class hui_item_querist{
                 }
                 $data[$item_num]["disable"] = $disabled;
                 $match_sta = 8;
-            }else if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-End-------\d*/is", $content)) && $match_sta === 8){
-                $item_num++;
-                $match_sta = 0;
+            }else if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-End-------\d*/is", $content))){
+                if($match_sta !== 8){
+                    return false;
+                }else{
+                    $item_num++;
+                    $match_sta = 0;
+                }
             }
         }
         fclose($file);
