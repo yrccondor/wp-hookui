08d4cda3f5573490e8f6d199242c6937399589ed
unknown <yrc371106@hotmail.com>
2018-05-17 11:28:47 +0800

    Add function data_to_array()

--- class-itemquerist.php
+++ class-itemquerist.php
@@ -34,5 +34,59 @@ class hui_item_querist{
     public function list_item(){
         //...
     }
+
+    /**
+     * Read data from target file, convert it to array
+     * return an array or FALSE
+     */
+    private function data_to_array(){
+        $file = fopen($this -> hui_target_file_name, "r");
+        $data = array();
+        $item_num = 0;
+        $match_sta = 0;
+        while(!feof($file)){
+            $content = fgets($file)
+            if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-Start-------\d*/is", $content)) && $match_sta === 0){
+                $data[$item_num] = array();
+                $match_sta = 1;
+            }else if($match_sta === 1){
+                $data[$item_num]["item_id"] = substr($content, 9);
+                $match_sta = 2;
+            }else if($match_sta === 2){
+                $data[$item_num]["hook_id"] = substr($content, 9);
+                $match_sta = 3;
+            }else if($match_sta === 3){
+                $data[$item_num]["action_id"] = substr($content, 11);
+                $match_sta = 4;
+            }else if($match_sta === 4){
+                $data[$item_num]["title"] = substr($content, 7);
+                $match_sta = 5;
+            }else if($match_sta === 5){
+                $data[$item_num]["content"] = substr($content, 9);
+                $match_sta = 6;
+            }else if($match_sta === 6){
+                $content_hash = substr($content, 14);
+                if(md5($data[$item_num]["content"]) !== $content_hash){
+                    return false;
+                }
+                $match_sta = 7;
+            }else if($match_sta === 7){
+                $disabled_content = substr($content, 9);
+                $disabled = false;
+                if($disabled_content === "True"){
+                    $disabled = true;
+                }else if($disabled_content !== "True" && $disabled_content !== "False"){
+                    return false;
+                }
+                $data[$item_num]["disable"] = $disabled;
+                $match_sta = 8;
+            }else if(preg_match_all("/\-------([0-9]|[A-z]){32}-([0-9]|[A-z]){7}-End-------\d*/is", $content)) && $match_sta === 8){
+                $item_num++;
+                $match_sta = 0;
+            }
+        }
+        fclose($file);
+        return $data;
+    }
 }
 ?>
